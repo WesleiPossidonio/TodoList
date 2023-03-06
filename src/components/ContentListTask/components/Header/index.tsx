e3d48531355136a4ef7c73822b579f6c8150f3c4
Weslei França <weslwifranca@gmail.com>
2023-03-06 14:30:16 -0300

    update pages

--- src/components/ContentListTask/components/Header/index.tsx
+++ src/components/ContentListTask/components/Header/index.tsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD:src/components/ContentListTask/components/Header/index.tsx
+
 import Styles from './header.module.css'
 import TodoListLogo from '../../../../assets/Logo-TodoList.svg'
-=======
-import Styles from './Header.module.css'
-import TodoListLogo from '../assets/Logo-TodoList.svg'
->>>>>>> origin:src/components/Header.tsx
 
 export const Header = () => {
   return (

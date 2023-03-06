8b902a160f3f8f0dc8fdfb0872d69b6de30a799d
Weslei França <weslwifranca@gmail.com>
2023-03-06 14:56:31 -0300

    add remove localstorage

--- src/components/ContentListTask/index.tsx
+++ src/components/ContentListTask/index.tsx
@@ -67,6 +67,8 @@ export function ContentListTak() {
     function handleDeleteTask(id: string) {
         const removeTask = tasks.filter((task) => task.id !== id)
 
+        localStorage.removeItem(TASK_LIST_STORAGE_KAY)
+
         setTasks(removeTask);
     }
 
